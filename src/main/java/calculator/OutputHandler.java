58c840d4d035b11a26c490e4766db1c96e24c7e3
sehako <dhtpgkr1999@gmail.com>
2025-10-17 17:21:49 +0900

    feat(output): 계산 결과 출력 구현

--- src/main/java/calculator/OutputHandler.java
+++ src/main/java/calculator/OutputHandler.java
@@ -2,8 +2,13 @@ package calculator;
 
 public class OutputHandler {
     private static final String GUIDE_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String RESULT_MESSAGE = "결과 : ";
 
     public static void printGuideMessage() {
         System.out.println(GUIDE_MESSAGE);
+    }
+
+    public static void printCalculateResult(int number) {
+        System.out.println(RESULT_MESSAGE + number);
     }
 }
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,5 +6,11 @@ public class StringCalculator {
     public void start() {
         OutputHandler.printGuideMessage();
         String input = Console.readLine();
+        int result = calculate(input);
+        OutputHandler.printCalculateResult(result);
+    }
+
+    private int calculate(String input) {
+        return 0;
     }
 }

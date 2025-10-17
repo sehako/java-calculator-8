9dfaf67b2b675b51d7a49651327e1f4a8447a972
sehako <dhtpgkr1999@gmail.com>
2025-10-17 16:42:27 +0900

    feat(output): 계산기 실행 시 초기 안내 문구 추가

---
+++ src/main/java/calculator/OutputHandler.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class OutputHandler {
+    private static final String GUIDE_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    public static void printGuideMessage() {
+        System.out.println(GUIDE_MESSAGE);
+    }
+}
